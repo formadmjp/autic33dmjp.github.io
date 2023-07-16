b75a67e18094d03408a0bd9f2fff493af238db16
formadjp <formadjp@gmail.com>
2023-07-15 19:04:16 -0600

    cambio

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyAAiqKtaixLowGvz89a0LpbwZPwlLWdUOM",
+  authDomain: "autic33dmjp.firebaseapp.com",
+  projectId: "autic33dmjp",
+  storageBucket: "autic33dmjp.appspot.com",
+  messagingSenderId: "923712088223",
+  appId: "1:923712088223:web:e2586ea2afc749375c9aaa",
+  measurementId: "G-86PJZCT1P4"
+    });
